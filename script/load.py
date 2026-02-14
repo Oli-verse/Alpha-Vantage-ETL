d71f88df112fccfa0af3b652995503b173fc74dc
Oliver Duzon <131516053+Oli-verse@users.noreply.github.com>
2026-02-14 21:30:38 +0800

    reset directory

--- script/load.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_data(df, file_path):
-    # Save the DataFrame to a CSV file
-    df.to_csv(file_path, index=True, index_label='Date')
-    print(f"Data loaded successfully to {file_path}")
